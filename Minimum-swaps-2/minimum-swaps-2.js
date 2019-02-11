1074772646184bec660aba8da968904eed040de2
Elias Makris <eliasimakris@gmail.com>
2019-02-11 20:24:16 +0100

    Array-swap challenge

---
+++ Minimum-swaps-2/minimum-swaps-2.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumSwaps function below.
+function minimumSwaps(arr) {
+
+    var length = arr.length;
+    var counter = 0;
+    var temp = 0;
+
+    for (var i = 0; i < length; i++) {
+        while (arr[i] != i + 1) {
+            temp = arr[i];
+            [arr[temp-1], arr[i]] = [arr[i], arr[temp-1]];
+            counter++;
+        }
+
+    }
+    return counter;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = minimumSwaps(arr);
+
+    ws.write(res + '\n');
+
+    ws.end();
+}
